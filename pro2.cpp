bad65afd31877e804903c82dbd81d6bb9a478020
a_wang <45402821+201165992@users.noreply.github.com>
2021-03-10 19:37:39 -0800

    Create pro2.cpp

---
+++ pro2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int id;
+  string first;
+  string last;
+  float score;
+  float avg;
+  cout << "Please enter your ID";
+  cin >> ID;
+  cout << "Please enter your First Name: ";
+  cin >> first; 
+  cout << "Please enter you Last Name: ";
+  cin >> last; 
+  cout << "Please enter your Score: ";
+  cin >> score;
+  avg = score / 3;
+  switch (avg) {
+    case (avg>=90): 
+      cout << "Average: " << avg; 
+      cout << "Grade: A\nComments: Congratulations on your acheivements!";
+      break;
+    case (90>avg>=80):
+      cout << "Average: " << avg;
+      cout << "Grade: B\nComments: ";
+      break;
+    case (80>avg>=70):
+      cout << "Average: " << avg;
+      cout << "Grade: C\nComments: ";
+      break;
+    case (70>avg>=60):
+      cout << "Average: " << avg;
+      cout << "Grade: D\nComments: ";
+      break;
+    case (60>avg):
+      cout << "Average: " << avg;
+      cout << "Grade: F\nComments: \nWarning: You Are Receiving a Failing Average for CS11, CS12, and CS22!"
+      break;
+  }
+}
